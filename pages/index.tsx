a1501575f474818523030811d0a65a8ee9e87492
Filipe Oliveira <hi@fmoliveira.dev>
2022-09-03 14:00:23 -0400

    Create POC analysis with compromise NLP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,87 @@
 import type { NextPage } from "next"
 import Head from "next/head"
+import { useForm } from "react-hook-form"
+import { useMutation } from "@tanstack/react-query"
+
+const importNlp = () => import("compromise/three")
+
+async function analyzeNLP(text: string) {
+	const importedModule = await importNlp()
+	const nlp = importedModule.default
+	const doc = nlp(text)
+	return Promise.resolve({
+		penn: doc.compute("penn").json(),
+		verbs: doc.verbs().json(),
+		adverbs: doc.adverbs().json(),
+		nouns: doc.nouns().json(),
+		adjectives: doc.adjectives().json(),
+	})
+}
+
+interface AnalyzeFormData {
+	text: string
+}
+
+const exampleText = `Having had some time at my disposal when in London, I had visited the British Museum, and made search among the books and maps in the library regarding Transylvania; it had struck me that some foreknowledge of the country could hardly fail to have some importance in dealing with a nobleman of that country. I find that the district he named is in the extreme east of the country, just on the borders of three states, Transylvania, Moldavia and Bukovina, in the midst of the Carpathian mountains; one of the wildest and least known portions of Europe. I was not able to light on any map or work giving the exact locality of the Castle Dracula, as there are no maps of this country as yet to compare with our own Ordnance Survey maps; but I found that Bistritz, the post town named by Count Dracula, is a fairly well-known place. I shall enter here some of my notes, as they may refresh my memory when I talk over my travels with Mina.`
 
 const Home: NextPage = () => {
+	const { handleSubmit, register } = useForm<AnalyzeFormData>({
+		defaultValues: {
+			text: exampleText,
+		},
+	})
+	const analysisMutation = useMutation((values: AnalyzeFormData) =>
+		analyzeNLP(values.text)
+	)
+
 	return (
-		<div>
+		<main className="p-2">
 			<Head>
 				<title>Grammalyzer</title>
+				<meta
+					name="description"
+					content="Experimenting with natural language processing."
+				/>
 			</Head>
-		</div>
+			<form
+				className="flex flex-col gap-2 max-w-3xl mx-auto my-10"
+				onSubmit={handleSubmit((values) => analysisMutation.mutate(values))}
+			>
+				<h1 className="text-2xl">Grammalyzer</h1>
+				<label
+					className="block text-gray-700 text-sm font-bold mb-2"
+					htmlFor="text"
+				>
+					Enter below the text you want to analyze
+				</label>
+				<textarea
+					{...register("text")}
+					id="text"
+					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					cols={30}
+					rows={10}
+				></textarea>
+				<button
+					type="submit"
+					className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
+				>
+					Analyze Text
+				</button>
+				{analysisMutation.isLoading && (
+					<div>Analysis in progress, please wait...</div>
+				)}
+				{analysisMutation.isError && (
+					<div className="text-red-500">
+						{JSON.stringify(analysisMutation.error)}
+					</div>
+				)}
+				{analysisMutation.isSuccess && (
+					<pre className="border border-gray-300 bg-gray-100 p-2 rounded whitespace-pre-wrap">
+						{JSON.stringify(analysisMutation.data, null, 2)}
+					</pre>
+				)}
+			</form>
+		</main>
 	)
 }
 
