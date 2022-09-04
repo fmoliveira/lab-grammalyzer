b763f395e39b77bedff53945ac70886b534c5c15
Filipe Oliveira <hi@fmoliveira.dev>
2022-09-04 01:26:11 -0400

    Move example trigger to button

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,14 @@ import { useForm } from "react-hook-form"
 import { useMutation } from "@tanstack/react-query"
 
 import Alert from "../components/Alert"
-import Button from "../components/Button"
 import DataTable from "../components/DataTable"
 import Form from "../components/Form"
 import Heading from "../components/Heading"
 import Label from "../components/Label"
-import Loader from "../components/Loader"
 import TextArea from "../components/TextArea"
 
 import { analyzeText } from "../lib/analyzeText"
+import Button from "../components/Button"
 import { exampleText } from "../lib/exampleText"
 
 interface AnalyzeFormData {
@@ -19,22 +18,26 @@ interface AnalyzeFormData {
 }
 
 const Home: NextPage = () => {
-	const form = useForm<AnalyzeFormData>({
-		defaultValues: {
-			text: exampleText,
-		},
-	})
+	const form = useForm<AnalyzeFormData>({})
 	const analysisMutation = useMutation((values: AnalyzeFormData) =>
 		analyzeText(values.text)
 	)
+
+	const showExampleText = () => {
+		form.setValue("text", exampleText)
+		analysisMutation.mutate({ text: exampleText })
+	}
 
 	return (
 		<div
 			className="grid w-screen h-screen"
 			style={{ gridTemplateColumns: "2fr 1fr", gridTemplateRows: "4rem auto" }}
 		>
-			<header className="col-span-2 p-4 flex flex-col justify-center bg-slate-900 text-slate-100">
+			<header className="col-span-2 p-4 flex flex-row items-center justify-between bg-slate-900 text-slate-100">
 				<Heading kind="h1">Grammalyzer</Heading>
+				<Button kind="primary" type="button" onClick={showExampleText}>
+					Show example text
+				</Button>
 			</header>
 			<main className="p-4 flex flex-col">
 				<Form
@@ -50,9 +53,6 @@ const Home: NextPage = () => {
 				</Form>
 			</main>
 			<aside className="p-4 border-l-2 border-slate-900 overflow-y-auto">
-				<Loader if={analysisMutation.isLoading}>
-					Analysis in progress, please wait...
-				</Loader>
 				<Alert if={analysisMutation.isError}>
 					{JSON.stringify(analysisMutation.error)}
 				</Alert>
